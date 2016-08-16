2470018500672370087b53e6a1a0db8531692dcd
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-08-16 10:28:55 -0400

    Done.

--- js/app/app.js
+++ js/app/app.js
@@ -1,2 +1,25 @@
 angular
-    .module('app', ['']);
+    .module('app', ['ui.router'])
+    .config(function($stateProvider){
+    	$stateProvider
+    		.state('home', {
+    			url: '/',
+    			templateUrl: 'views/home.html',
+    			controller: 'HomeController as home'
+    		})
+    		.state('home.timeline', {
+    			url: 'timeline',
+    			templateUrl: 'views/home/timeline.html',
+    			controller: 'HomeController as home'
+    		})
+    		.state('home.user', {
+    			url: 'user',
+    			templateUrl: 'views/home/user.html',
+    			controller: 'HomeController as home'
+    		})
+    		.state('home.notifications', {
+    			url: 'notifications',
+    			templateUrl: 'views/home/notifications.html',
+    			controller: 'HomeController as home'
+    		});
+    });
